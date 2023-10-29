c567d501786d7e50466963bba7437acf0e2d4356
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-10-29 14:03:21 +0000

    No sé

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import BodyCryptPlanet from "./component/body";
-import NavBarCryptPlanet from "./component/navbar";
-import FooterCryptPlanet from "./component/footer";
+import BodyCryptPlanet from "./body";
+import NavBarCryptPlanet from "./navbar";
+import FooterCryptPlanet from "./footer";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React from 'react';
+import '../../styles/index.css';
 
-//create your first component
 const NavBarCryptPlanet = () => {
-	return (<>
-		<button className="btn btn-success">qwe</button>
-	</>);
-};
+  return (<>
+		<div className="container-fluid" id="navbar">
+			<div className="navbar__brand">
+				<span style={{color: "white"}}>Crypt&nbsp;
+				<i class="fa-solid fa-earth-americas fa-xl"></i>&nbsp;Planet</span>
+			</div>
+			<div className="navbar__options">
+				<a href="#">Discover</a>
+				<a href="#">Collection</a>
+				<a href="#">Artist</a>
+			</div>
+		</div>
+	</>)
+}
 
 export default NavBarCryptPlanet;
